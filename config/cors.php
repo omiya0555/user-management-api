6dea02051f053d2cf5da5b08fee6d5678e55ee8d
omiya0555 <otoharadak@gmail.com>
2024-09-25 22:11:13 +0900

    Fix typo in cors.php.
    forgot the , in 20.

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*']
+    'allowed_methods' => ['*'],
 
     //'allowed_origins' => ['*'],
     // Sanctumが正しく動作するためCORSの設定
